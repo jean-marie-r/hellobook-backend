db843a9cfef0cd6ffeea33c2054248a7139b5775
Jean-Marie <jean@jean.local>
2025-05-05 15:11:17 +0200

    add HealthController in controller module

--- src/controllers/controller.module.ts
+++ src/controllers/controller.module.ts
@@ -4,13 +4,15 @@ import { Module } from '@nestjs/common';
 import { IdGeneratorModule } from 'src/infrastructure/providers/id-generator/id-generator.module';
 
 import { UsersController } from './users.controller';
+import { HealthController } from './health.controller'
 
 import { FindUserUseCase } from 'src/core/use-cases/users/find-user.usecase';
 import { CreateUserUseCase } from 'src/core/use-cases/users/create-user.usecase';
 
+
 @Module({
     imports: [IdGeneratorModule],
-    controllers: [UsersController],
+    controllers: [HealthController, UsersController],
     providers: [CreateUserUseCase, FindUserUseCase],
 })
 export class ControllerModule { }

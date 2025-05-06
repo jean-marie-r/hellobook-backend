ab545a4b48fa1d1d9397507541c7bd919cd6e3ec
Jean-Marie <jean@jean.local>
2025-05-06 10:50:55 +0200

    update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,28 +2,9 @@
 
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 2
-done
-
->&2 echo "Postgres is up - executing command"
-
-# Run migrations
-npm run prisma:generate
-
-if [ -z "$(ls -A prisma/migrations)" ]; then
-    npx prisma migrate dev --name init
-else
-    npm run prisma:migrate
-fi
-
 # Start the application
 if [ "$NODE_ENV" = "production" ]; then
   npm run start:prod
 else
   npm run start:dev
 fi
-
-# Clean up
-unset PGPASSWORD
